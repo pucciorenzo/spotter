9018fa7169bb660ed60e6c53c09b359fea94fa84
Vincenzo Cosi <46821670+pucciorenzo@users.noreply.github.com>
2026-05-19 09:05:21 +0200

    feat: add SwiftData models and iPhone CRUD (#2)

--- iOSApp/App/SpotterApp.swift
+++ iOSApp/App/SpotterApp.swift
@@ -1,10 +1,22 @@
 import SwiftUI
+import SwiftData
 
 @main
 struct SpotterApp: App {
+    private let modelContainer: ModelContainer
+
+    init() {
+        do {
+            modelContainer = try ModelContainerProvider.make()
+        } catch {
+            fatalError("Unable to create SwiftData model container: \(error)")
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
             RootTabView()
         }
+        .modelContainer(modelContainer)
     }
 }
